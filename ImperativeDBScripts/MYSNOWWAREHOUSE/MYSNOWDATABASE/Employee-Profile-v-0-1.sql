7496f7c0b44dc6efa6c0b09f7667e3385c1779c8
Prakash Posam <prakashbabu01@gmail.com>
2024-08-01 15:07:58 -0400

    Updated the column name

--- ImperativeDBScripts/MYSNOWWAREHOUSE/MYSNOWDATABASE/Employee-Profile-v-0-1.sql
+++ ImperativeDBScripts/MYSNOWWAREHOUSE/MYSNOWDATABASE/Employee-Profile-v-0-1.sql
@@ -6,7 +6,7 @@ USE DATABASE MYSNOWDATABASE;
 
 USE SCHEMA MYSNOWSCHEMA;
 
-Alter table EMPLOYEE_PROFILE ADD Column office_phone varchar;
+Alter table EMPLOYEE_PROFILE ADD Column office_phone_num varchar;
 
 USE ROLE SECURITYADMIN;
 
