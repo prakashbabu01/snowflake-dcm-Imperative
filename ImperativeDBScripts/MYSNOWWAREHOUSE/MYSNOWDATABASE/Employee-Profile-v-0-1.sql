68d09c0b12d7a8dfbd0783fa096e2731d880c433
Prakash Posam <prakashbabu01@gmail.com>
2024-08-01 15:01:06 -0400

    udpated column name

--- ImperativeDBScripts/MYSNOWWAREHOUSE/MYSNOWDATABASE/Employee-Profile-v-0-1.sql
+++ ImperativeDBScripts/MYSNOWWAREHOUSE/MYSNOWDATABASE/Employee-Profile-v-0-1.sql
@@ -6,7 +6,7 @@ USE DATABASE MYSNOWDATABASE;
 
 USE SCHEMA MYSNOWSCHEMA;
 
-Alter table EMPLOYEE_PROFILE ADD Column home_address varchar;
+Alter table EMPLOYEE_PROFILE ADD Column office_phone varchar;
 
 USE ROLE SECURITYADMIN;
 
